dbc4c420944a37819611283ce143a89a8b6b9247
sakura1ka555-ai <sakura1ka555@gmail.com>
2026-09-16 20:06:49 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <div className="app">
+      <div className="logo">
+        VIBE
+      </div>
+
+      <h1>
+        Смотри вместе
+      </h1>
+
+      <p>
+        Совместный просмотр фильмов
+        с друзьями на расстоянии
+      </p>
+
+      <button>
+        Создать комнату
+      </button>
+
+      <button className="secondary">
+        Войти в комнату
+      </button>
+    </div>
+  );
+}
+
+export default App;
