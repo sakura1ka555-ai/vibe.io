cbbeb51a8c9fa69a386347eb2949aa4595d60917
sakura1ka555-ai <sakura1ka555@gmail.com>
2026-09-16 20:31:08 +0300

    Update telegram.ts

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,11 +1,44 @@
-export function getTelegramUser() {
-  const tg = window.Telegram?.WebApp;
+export function initTelegram() {
+
+  const tg =
+    window.Telegram?.WebApp;
+
 
   if (!tg) {
+
+    console.log(
+      "Telegram WebApp не найден"
+    );
+
     return null;
+
   }
+
 
   tg.ready();
 
+  tg.expand();
+
+
+  return tg;
+
+}
+
+
+
+export function getTelegramUser() {
+
+  const tg =
+    window.Telegram?.WebApp;
+
+
+  if (!tg) {
+
+    return null;
+
+  }
+
+
   return tg.initDataUnsafe?.user || null;
+
 }
