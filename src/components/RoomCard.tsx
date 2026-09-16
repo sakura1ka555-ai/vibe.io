f1a038204bc579f9996651e084a7e47961334115
sakura1ka555-ai <sakura1ka555@gmail.com>
2026-09-16 20:09:59 +0300

    Create RoomCard.tsx

---
+++ src/components/RoomCard.tsx
@@ -0,0 +1,22 @@
+type RoomCardProps = {
+  title: string;
+  users: number;
+};
+
+function RoomCard({ title, users }: RoomCardProps) {
+  return (
+    <div className="room-card">
+      <h2>{title}</h2>
+
+      <p>
+        👥 {users} участников
+      </p>
+
+      <button>
+        Войти
+      </button>
+    </div>
+  );
+}
+
+export default RoomCard;
